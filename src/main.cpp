8b71595211aba1f8968451ffb7a1721ecd428db6
Federico Fuga <fuga@studiofuga.com>
2021-10-18 11:19:26 +0200

    Barebone project withBME280 and CCS811 integration

---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+#include <Arduino.h>
+#include <Wire.h>    // I2C library
+#include "ccs811.h"  // CCS811 library
+#include <BME280I2C.h>
+
+CCS811 *ccs811;
+BME280I2C *bme;
+
+void setupIO()
+{
+    Wire.begin(21, 22);
+}
+
+void setupAirQ()
+{
+    ccs811 = new CCS811(23, CCS811_SLAVEADDR_1); // nWAKE on 23
+    
+    // Enable CCS811
+    ccs811->set_i2cdelay(50); // Needed for ESP8266 because it doesn't handle I2C clock stretch correctly
+    bool ok= ccs811->begin();
+    if( !ok ) 
+        Serial.println("setup: CCS811 begin FAILED");
+
+    // Print CCS811 versions
+    Serial.print("setup: hardware    version: "); Serial.println(ccs811->hardware_version(),HEX);
+    Serial.print("setup: bootloader  version: "); Serial.println(ccs811->bootloader_version(),HEX);
+    Serial.print("setup: application version: "); Serial.println(ccs811->application_version(),HEX);
+
+    // Start measuring
+    ok= ccs811->start(CCS811_MODE_1SEC);
+    if( !ok ) Serial.println("setup: CCS811 start FAILED");
+}
+
+void setupTemp()
+{
+    BME280I2C::Settings settings;
+    settings.bme280Addr = BME280I2C::I2CAddr_0x77;
+    bme = new BME280I2C(settings);
+
+    while(!bme->begin())
+    {
+        Serial.println("Could not find BME280 sensor!");
+        delay(1000);
+    }
+
+    switch(bme->chipModel())
+    {
+        case BME280::ChipModel_BME280:
+            Serial.println("Found BME280 sensor! Success.");
+            break;
+        case BME280::ChipModel_BMP280:
+            Serial.println("Found BMP280 sensor! No Humidity available.");
+            break;
+        default:
+            Serial.println("Found UNKNOWN sensor! Error!");
+    }
+}
+
+void setupSerial()
+{
+    // Enable serial
+    Serial.begin(115200);
+    Serial.println("");
+    Serial.println("setup: Starting CCS811 basic demo");
+    Serial.print("setup: ccs811 lib  version: "); Serial.println(CCS811_VERSION);
+}
+
+void setup()
+{
+    setupSerial();
+    setupIO();
+    setupAirQ();
+    setupTemp();
+}
+
+void readAirQ()
+{
+    // Read
+    uint16_t eco2, etvoc, errstat, raw;
+    ccs811->read(&eco2,&etvoc,&errstat,&raw);
+
+    // Print measurement results based on status
+    if( errstat==CCS811_ERRSTAT_OK ) {
+        Serial.print("CCS811: ");
+        Serial.print("eco2=");  Serial.print(eco2);     Serial.print(" ppm  ");
+        Serial.print("etvoc="); Serial.print(etvoc);    Serial.print(" ppb  ");
+        //Serial.print("raw6=");  Serial.print(raw/1024); Serial.print(" uA  ");
+        //Serial.print("raw10="); Serial.print(raw%1024); Serial.print(" ADC  ");
+        //Serial.print("R="); Serial.print((1650*1000L/1023)*(raw%1024)/(raw/1024)); Serial.print(" ohm");
+        Serial.println();
+    } else if( errstat==CCS811_ERRSTAT_OK_NODATA ) {
+        Serial.println("CCS811: waiting for (new) data");
+    } else if( errstat & CCS811_ERRSTAT_I2CFAIL ) {
+        Serial.println("CCS811: I2C error");
+    } else {
+        Serial.print("CCS811: errstat="); Serial.print(errstat,HEX);
+        Serial.print("="); Serial.println( ccs811->errstat_str(errstat) );
+    }
+}
+
+void readTemp()
+{
+    float temp(NAN), hum(NAN), pres(NAN);
+
+    BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+    BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+
+    bme->read(pres, temp, hum, tempUnit, presUnit);
+
+    Serial.print("Temp: ");
+    Serial.print(temp);
+    Serial.print("°"+ String(tempUnit == BME280::TempUnit_Celsius ? 'C' :'F'));
+    Serial.print("\t\tHumidity: ");
+    Serial.print(hum);
+    Serial.print("% RH");
+    Serial.print("\t\tPressure: ");
+    Serial.print(pres);
+    Serial.println("Pa");
+}
+
+void loop()
+{
+    readAirQ();
+    readTemp();
+
+    delay(1000);
+}
